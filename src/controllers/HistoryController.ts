b5df6e39173ee01abd033a27c8e34955674fdace
cahyo arissabarno <cahyoarissabarno@gmail.com>
2023-06-07 19:09:04 +0700

    update history and filter by date range

--- src/controllers/HistoryController.ts
+++ src/controllers/HistoryController.ts
@@ -3,6 +3,7 @@ import { requestHandler } from "../utils/RequestHandler";
 import { config as dotenv } from "dotenv";
 import ErrorHandler from "../utils/ErrorHandler";
 import axios from "axios";
+import { Op } from 'sequelize';
 
 const Block = require('../models').Block;
 
@@ -12,15 +13,96 @@ class HistoryController {
     read = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
         try {
             const data = req.body;
-            axios.get(`http://10.0.2.7:8181/device-test/log/${data.page}?from=${data.address}&device_id=${data.device_id}`)
+            const itemsPerPage = 5; // Jumlah item per halaman
+            const currentPage = data.page; // Halaman saat ini
+            const offset = (currentPage - 1) * itemsPerPage;
+
+            const blocks = await Block.findAll({
+                where: {
+                    device_id: data.device_id
+
+                    // device_id: req.body.device_id,
+                    // block_number: {
+                    //     [Op.gte]: 5
+                    // }
+                },
+                limit: itemsPerPage,
+                offset: offset,
+                order: [['block_number', 'ASC']]
+            })
+
+            const jsonResult = blocks.map((block:any) => block.toJSON());
+            const pageData = jsonResult.sort((a: any, b:any) => a.block_number - b.block_number);
+            const fromBlock = pageData[0].block_number;
+            const toBlock = pageData[pageData.length - 1].block_number;
+            console.log(pageData)
+            console.log(fromBlock, toBlock);
+            // res.status(200).send({from: pageData[0].block_number, to: pageData[4].block_number});
+            // res.status(200).send(pageData);
+            
+            // axios.get(`http://10.0.2.7:8181/device-test/log/${data.page}?from=${data.address}&device_id=${data.device_id}&fromBlock=${fromBlock}&toBlock=${toBlock}`)
+            axios.get(`http://localhost:8080/device-test/log/${data.page}?from=${data.address}&device_id=${data.device_id}&fromBlock=${fromBlock}&toBlock=${toBlock}`)
             .then(function (response) {
-                console.log(response)
+                // console.log(response)
                 res.status(200).send(response.data);
             })
             .catch(function (error) {
                 console.log(error)
                 res.status(500).send(error);
             });
+            
+            // console.log(find)
+        } catch (e) {
+            console.log(e)
+            return res.status(500).send((e as Error));;
+        }
+    }
+
+    readByDate = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
+        try {
+            const data = req.body;
+            const itemsPerPage = 5; // Jumlah item per halaman
+            const currentPage = data.page; // Halaman saat ini
+            const offset = (currentPage - 1) * itemsPerPage;
+
+            const blocks = await Block.findAll({
+                where: {
+                    device_id: data.device_id,
+
+                    // device_id: req.body.device_id,
+                    // block_number: {
+                    //     [Op.gte]: 5
+                    // }
+                    date: {
+                        [Op.between]: [data.fromDate, data.toDate]
+                    }
+                },
+                limit: itemsPerPage,
+                offset: offset,
+                order: [['block_number', 'ASC']]
+            })
+
+            const jsonResult = blocks.map((block:any) => block.toJSON());
+            const pageData = jsonResult.sort((a: any, b:any) => a.block_number - b.block_number);
+            const fromBlock = pageData[0].block_number;
+            const toBlock = pageData[pageData.length - 1].block_number;
+            console.log(pageData)
+            console.log(fromBlock, toBlock);
+            // res.status(200).send({from: pageData[0].block_number, to: pageData[4].block_number});//
+            // res.status(200).send(blocks);
+            
+            axios.get(`http://10.0.2.7:8181/device-test/log/${data.page}?from=${data.address}&device_id=${data.device_id}&fromBlock=${fromBlock}&toBlock=${toBlock}`)
+            // axios.get(`http://localhost:8080/device-test/log/${data.page}?from=${data.address}&device_id=${data.device_id}&fromBlock=${fromBlock}&toBlock=${toBlock}`)
+            .then(function (response) {
+                // console.log(response)
+                res.status(200).send(response.data);
+            })
+            .catch(function (error) {
+                console.log(error)
+                res.status(500).send(error);
+            });
+            
+            // console.log(find)
         } catch (e) {
             console.log(e)
             return res.status(500).send((e as Error));;
